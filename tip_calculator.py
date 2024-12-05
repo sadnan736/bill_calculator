45b85d697dfa30266b6009c3cb34022bd0cb5239
Sadnan Islam <sadnan15916q@gmail.com>
2024-12-05 23:49:37 +0600

    add default values for tip, split default tip of 25% would be added if no input is given and also if no input is given for splitting bills, it will automatically be split for one person

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,9 +1,22 @@
 print("Welcome to the Tip Calcutator!")
-    bill = float(input("What was the total bill? ৳"))
-persons = int(input("How many people will split the bill? "))
-tip = float((input("How much tip would you like to give? Standard is 15% ")))
 
-pperson_pay = (bill + (bill * tip / 100)) / persons
-pperson_pay = round(pperson_pay, 2)
+def bill_split():
+    tot_p = (input("How many people will split the bill? "))
+    tot_p = 1 if tot_p == '' else int(tot_p)
 
-print(f"Each person should pay: ৳{pperson_pay}")
+    return tot_p
+
+def tip_calc():
+    i_tip = (input("How much tip would you like to give? Standard is 15% "))
+    i_tip = 25 if i_tip == '' else float(i_tip)
+
+    return i_tip
+
+bill = float(input("What was the total bill? ৳"))
+persons = bill_split()
+tip = tip_calc()
+
+per_person_pay = (bill + (bill * tip / 100)) / persons
+per_person_pay = round(per_person_pay, 2)
+
+print(f"Each person should pay: ৳{per_person_pay}")
