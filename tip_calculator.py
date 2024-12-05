9daf879c2c657c135f3b0a9d8807053010ab891e
Sadnan Islam <sadnan15916q@gmail.com>
2024-12-05 23:30:25 +0600

    remove tkinter residue code

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,14 +1,5 @@
-from tkinter import *
-
-root = Tk()
-
-mylabel = Label(root, text='somethiong')
-mylabel.pack()
-
-root.mainloop()
-
 print("Welcome to the Tip Calcutator!")
-bill = float(input("What was the total bill? ৳"))
+    bill = float(input("What was the total bill? ৳"))
 persons = int(input("How many people will split the bill? "))
 tip = float((input("How much tip would you like to give? Standard is 15% ")))
 
